c76082f9376c277cdc3766c8151968db7e3c01f5
bintanghr <haridaramadhan@gmail.com>
2022-05-23 20:04:47 +0700

    inisialisasi project

---
+++ Dosen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUts
+{
+    internal class Dosen
+    {
+        //private variable
+        private int id;
+        private string nama;
+        private int nik;
+        private string gender;
+        private string course;
+
+        //properti
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public string Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        //method
+        public Dosen()
+        {
+            id = 554273;
+            nama = "Kamarudin";
+            nik = 21114325;
+            gender = "Laki-laki";
+            course = "Pemrograman";
+            
+            Console.WriteLine("Nama dosen : {0}", this.nama);
+            Console.WriteLine("Jenis Kelamin : {0}", this.gender);
+            Console.WriteLine("Nomor Id : {0}", this.id);
+            Console.WriteLine("Nomor Nik : {0}", this.nik);
+            Console.WriteLine("Mata kuliah yang diampu : {0}", this.course);
+            Console.WriteLine();
+        }
+        public void AddDosen(int id, string nama, int nik, string gender)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Penambahan data diri dosen : ");
+            Console.WriteLine("Nama dosen : {0}", Nama);
+            Console.WriteLine("Jenis Kelamin : {0}", Gender);
+            Console.WriteLine("Nomor Id : {0}", Id);
+            Console.WriteLine("Nomor Nik : {0}", Nik);
+        }
+
+        public void AddCourse(string course)
+        {
+            Console.WriteLine("Mata kuliah yang diampu : {0}", Course);
+            Console.WriteLine("Berhasil !");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen();
+
+            Console.WriteLine("Data diri dosen baru : ");
+            Console.Write("Masukkan nama dosen : ");
+            dosen1.Nama = Console.ReadLine();
+            Console.Write("Jenis kelamin : ");
+            dosen1.Gender = Console.ReadLine();
+            Console.Write("Nomor id : ");
+            dosen1.Id = int.Parse(Console.ReadLine());
+            Console.Write("Nomor NIK : ");
+            dosen1.Nik = int.Parse(Console.ReadLine());
+            Console.Write("Mata kuliah yang diampu : ");
+            dosen1.Course = Console.ReadLine();
+
+            dosen1.AddDosen(dosen1.Id, dosen1.Nama, dosen1.Nik, dosen1.Gender);
+            dosen1.AddCourse(dosen1.Course);
+
+            Console.ReadKey();
+        }
+    }
+}
